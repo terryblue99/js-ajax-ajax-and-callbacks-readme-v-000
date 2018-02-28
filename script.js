205e6da22e1bc3d8f372ae10e3189cb94b69c6cb
terryblue99 <terryblue@comcast.net>
2018-02-27 18:01:04 -0600

    Done.

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
     // inserting the response
     $("#sentences").html(response);
   });
+
+  $.get("this_doesnt_exist.html", function(data) {
+    // This will not be called because the .html file request doesn't exist
+    doSomethingGood();
+  }).fail(function(error) {
+   // This is called when an error occurs
+   console.log('Something went wrong: ' + error);
+  });
+
 });
+
+// This is another example of how we could use jQuery to perform an Ajax request
+
+var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+
+$.get(url)
+  .done(function(data) {
+    console.log("Done");
+    console.log(data);
+  });
